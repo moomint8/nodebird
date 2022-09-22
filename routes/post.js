0809e38878b09a5eeda64efb24305e7fa6599359
moomint8 <office05053@gmail.com>
2022-09-22 20:05:59 +0900

    22/09/22 20:05

--- routes/post.js
+++ routes/post.js
@@ -2,14 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
 const { Post, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
-
-const AWS = require('aws-sdk');
-const multerS3 = require('multer-s3');
 
 try {
     fs.readdirSync('uploads');
@@ -23,11 +22,10 @@ AWS.config.update({
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     region: 'ap-northeast-2',
 });
-
 const upload = multer({
     storage: multerS3({
         s3: new AWS.S3(),
-        bucket: 'nodebirdmoomint',
+        bucket: 'nodebird',
         key(req, file, cb) {
             cb(null, `original/${Date.now()}${path.basename(file.originalname)}`);
         },
